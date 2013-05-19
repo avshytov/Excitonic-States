f0be2eba22e826724f023009967ccac175f613d3
avshytov <avshytov@gmail.com>
2013-05-19 08:54:40 +0100

    Cleanup
    - Code cleanup
    - Obsolete slow routines removed
    - Nx, Ny, etc are hard-coded

--- LDOS.py
+++ LDOS.py
@@ -7,26 +7,32 @@ from matplotlib import rc
 rc('text', usetex = True)
 rc('font', family = 'serif')
 
-Nx = int(input("Nx = "))
-Ny = int(input("Ny = "))
-pxmax = float(input("PxMax = "))
-pymax = float(input("PyMax = "))
-gamma = float(input("gamma = "))
+Nx = 50
+Ny = 50
+pxmax = 3.14
+pymax = 3.14
+gamma = 0.05
+
+if False:
+	Nx = int(input("Nx = "))
+	Ny = int(input("Ny = "))
+	pxmax = float(input("PxMax = "))
+	pymax = float(input("PyMax = "))
+	gamma = float(input("gamma = "))
+	
 N= 2 * Nx * Ny				#calculate size of matrix
 
 H0 = np.zeros((N,N))		#allocate empty matrix
 V = np.zeros((N,N))
 H = np.zeros((N,N))
 
-deltaPx = 2.0*float(pxmax)/float(Nx - 1.0)
-deltaPy = 2.0*float(pymax)/float(Ny - 1.0)
-alpha = 1.0
-Calpha = alpha * float(deltaPx * deltaPy) / 2.0 / math.pi
+px = np.linspace(-pxmax, pxmax, Nx)
+py = np.linspace(-pymax, pymax, Ny)
+deltaPx = px[1] - px[0]
+deltaPy = py[1] - py[0]
+alpha = 2.0
+Cv = float(deltaPx * deltaPy) / 2.0 / math.pi
 
-ixvals = np.array(range(Nx))
-iyvals = np.array(range(Ny))
-px = pxmax * (ixvals * 2.0 / float(Nx - 1) - 1.0)
-py = pymax * (iyvals * 2.0 / float(Ny - 1) - 1.0)
 
 #potential energy terms
 for iX1 in range (Nx):			#1,-0.8		0.005,
@@ -37,88 +43,77 @@ for iX1 in range (Nx):			#1,-0.8		0.005,
 				i0 = (iX1*Ny + iY1)*2
 				j0 = (iX2*Ny + iY2)*2
 
-				px1 = px[iX1] #pxmax * ((iX1*2.0)/float(Nx - 1) - 1.0)
-				px2 = px[iX2] #pxmax * ((iX2*2.0)/float(Nx - 1) - 1.0)
+				px1 = px[iX1] 
+				px2 = px[iX2] 
 				
-				py1 = py[iY1] #pymax * ((iY1*2.0)/float(Ny - 1) - 1.0)
-				py2 = py[iY2] #pymax * ((iY2*2.0)/float(Ny - 1) - 1.0)
+				py1 = py[iY1] 
+				py2 = py[iY2] 
 
-				phi1 = np.arctan2(py1,px1)
-				phi2 = np.arctan2(py2,px2)
+				phi1 = np.arctan2(py1, px1)
+				phi2 = np.arctan2(py2, px2)
 
-				if px1==px2 and py1==py2:			#avoid division by zero
-					modulus = 0.5 * math.sqrt( deltaPx ** 2 + deltaPy ** 2 )
-
+				# Never, ever use a == b with floating 
+				# point values!
+				if iX1 == iX2 and iY1 == iY2:			#avoid division by zero
+					q = 0.5 * math.sqrt( deltaPx ** 2 + deltaPy ** 2 )
 				else:
-					modulus = math.sqrt((px1-px2)**2+(py1-py2)**2)
+				        q = math.sqrt((px1-px2)**2+(py1-py2)**2)
 
-				if i0<=N:							#stops overfilling of matrix
-					if j0<=N:
-						V[i0,j0] = - (1.0 / modulus)* np.cos(phi1 - phi2)
-						V[j0,i0] = - (1.0 / modulus)* np.cos(phi1 - phi2) 
+				cosphi = math.cos( phi1 - phi2 ) # not np.cos!
+				Uq = -1.0 / q
+				
+				V[i0,j0] = Uq * cosphi
+				V[j0,i0] = V[i0, j0] 
 
-				if i0+1<=N:
-					if j0+1<=N:
-						V[j0+1,i0+1] = - 1.0 / modulus
+				V[j0 + 1, i0 + 1] = Uq
+				V[i0 + 1, j0 + 1] = V[j0 + 1, i0 + 1]
 
-V *= Calpha
+V *= Cv
 
 #kinetic energy terms
 for iX1 in range (Nx):
 	for iY1 in range (Ny):
-
-				i0 = (iX1*Ny + iY1)*2
-
-				px1 = px[iX1] #pxmax * ((iX1*2.0)/float(Nx - 1) - 1.0)
-				py1 = py[iY1] #pxmax * ((iY1*2.0)/float(Ny - 1) - 1.0)
+		i0 = (iX1*Ny + iY1)*2
+		px1 = px[iX1] 
+		py1 = py[iY1] 
 				
-				p=math.sqrt(px1**2+py1**2)
-				phi1 = np.arctan2(py1,px1)
-
-				H0[i0,i0+1] = p**2 * np.cos(3 * phi1)
-				H0[i0+1,i0] = p**2 * np.cos(3 * phi1)
-
-
-H=V+H0
+		p    = math.sqrt(px1**2+py1**2)
+		phi1 = np.arctan2(py1,px1)
+				
+		if True:
+			Ep = p**2 * math.cos( 3.0 * phi1 )
+		else:
+			Ep = p**2
+					
+		H0[ i0,     i0 + 1 ] = Ep
+		H0[ i0 + 1, i0     ] = Ep
+				
+H = H0 + alpha * V
 
 print ("Diagonalising...")
 (E,a) = lin.eigh(H)		#calculate eigenvalues, a are the row eigenvectors
 print ("done")
 
 
-for icheck in [ 0, N-1, N/2]:
-    Echeck = E[icheck]
-    ev = a[:, icheck]
-    print ("icheck = ", icheck, "norm:",  lin.norm(np.dot(H, ev) - Echeck * ev)) 
+if False:
+	for icheck in [ 0, N-1, N/2]:
+		Echeck = E[icheck]
+		ev = a[:, icheck]
+		print ("icheck = ", icheck, "norm:",  lin.norm(np.dot(H, ev) - Echeck * ev)) 
 
 theta = 0.0
 
-Emin = -3.0 #min(E)
-Emax = +3.0 #max(E)
-dE = (Emax - Emin) / 200.0
-Evals = np.arange (Emin, Emax, dE)
+Emin = -5.0 
+Emax = +5.0 
+nE = 500
+Evals = np.linspace (Emin, Emax, nE)
 
 Rmin = 0.0
 Rmax = 5.0
-dR = (Rmax - Rmin) / 200.0
-Rvals = np.arange (Rmin, Rmax, dR)
+nR = 200
+Rvals = np.linspace (Rmin, Rmax, nR)
 
 print ("Transforming eigenvectors to R-space")
-#find the eigenvectors in position space
-if False:
-	psi1 = np.zeros((N,len(Rvals)), dtype=complex)
-	psi2 = np.zeros((N,len(Rvals)), dtype=complex)
-	for n in range (len(E)):				#steps through eigenvalues
-		for R in range (len(Rvals)):
-			for iX in range (Nx):
-				for iY in range (Ny):
-					i = (iX*Ny + iY)*2				#lables momentum state
-					px_i = px[iX] #pxmax * ((iX*2.0)/float(Nx - 1) - 1.0)
-					py_i = py[iY] #pymax * ((iY*2.0)/float(Ny - 1) - 1.0)
-					pr = (px_i * math.cos(theta) + py_i * math.sin(theta)) * Rvals[R]
-					exp_pr = math.cos(pr) + 1j * math.sin(pr)
-					psi1[n,R] += a[i,   n] * exp_pr
-					psi2[n,R] += a[i+1 ,n] * exp_pr
 
 print ("Generating the Unitary transform")
 U = np.zeros((len(Rvals), N/2), dtype=complex)
@@ -135,19 +130,13 @@ print ("Doing unitary transformation to the R space")
 psi3 = np.zeros((N,len(Rvals)), dtype=complex)
 psi4 = np.zeros((N,len(Rvals)), dtype=complex)
 for n in range (len(E)):
-	psi3[n, :] = np.dot(U,  a[::2, n])
+	psi3[n, :] = np.dot(U,  a[ ::2, n])
 	psi4[n, :] = np.dot(U,  a[1::2, n])
-	#print ("n = ", n, "diff: ", lin.norm(psi3[n, :] - psi1[n, :]), lin.norm(psi4[n, :] - psi2[n, :]))
-	#print ("psi1 = ", psi1, "psi2 = ", psi3)
 
 print ("done")
 print ("Calculating LDOS")
 #calculate the probability densities
 probDensity = np.abs(psi3)**2 + np.abs(psi4)**2
-#probDensity = np.zeros((N,len(Rvals)))
-#for n in range (len(E)):
-#	for R in range (len(Rvals)):
-#		probDensity[n,R] = abs( psi1[n,R] )**2  + abs( psi2[n, R] )**2
 
 #calculate LDOS
 LDOS = np.zeros((len(Evals), len(Rvals)))
@@ -178,7 +167,7 @@ print ("Creating color plot")
 mpl.figure()
 mpl.pcolor(XX, YY, LDOS)
 mpl.colorbar()
-mpl.title('LDOS, alpha = %g' % alpha)
+mpl.title(r'LDOS, $\alpha = %g$' % alpha)
 mpl.xlabel(r'Distance $R$')
 mpl.ylabel(r'Energy $\epsilon$')
 
